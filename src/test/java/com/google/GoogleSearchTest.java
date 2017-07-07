560682eb23e237c02f008e78cefa0af845afccce
CoffeeInka <inna.shevchenko@gmail.com>
2017-07-07 17:48:15 +0200

    first line in selenium

--- src/test/java/com/google/GoogleSearchTest.java
+++ src/test/java/com/google/GoogleSearchTest.java
@@ -1,7 +1,13 @@
 package com.google;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * Created by inna on 07/07/2017.
  */
 public class GoogleSearchTest {
+
+    WebDriver driver = new FirefoxDriver();
+    driver.get("http://www.google.com");
 }
