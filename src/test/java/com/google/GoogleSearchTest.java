c3dfd8491373312d599c32195e0a042030cf79d1
CoffeeInka <inna.shevchenko@gmail.com>
2017-07-09 09:42:41 +0200

    Webdriver wait fails with exceptionInInitializerError

--- src/test/java/com/google/GoogleSearchTest.java
+++ src/test/java/com/google/GoogleSearchTest.java
@@ -1,14 +1,16 @@
 package com.google;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
@@ -18,47 +20,65 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.*;
  */
 public class GoogleSearchTest {
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeClass
+    public static void setUp() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("marionette", false);
         driver = new FirefoxDriver(capabilities);
     }
 
-    @After
-    public void tearDown() {
+    @AfterClass
+    public static void tearDown() {
         driver.quit();
     }
 
-    WebDriver driver;
+    public static WebDriver driver;
+    public static WebDriverWait wait = new WebDriverWait(driver, 6);
 
     @Test
     public void testSearchThenFollowLink() throws Exception {
         driver.get("http://google.com");
         search("Selenium automates browsers");
         assertResultsAmount(10);
-        assertFirstResultHasText("for automating web applications for testing purposes");
+        assertResult(1, "for automating web applications for testing purposes");
 
-        followFirstLink();
-        new WebDriverWait(driver, 6).until(visibilityOfAllElementsLocatedBy(By.cssSelector("#mainContent>h2")));
-        assertEquals(driver.getCurrentUrl(), "http://www.seleniumhq.org/");
+        followLink(0);
+        wait.until(urlContains("http://www.seleniumhq.org/"));
     }
 
-    private void search(String query) {
+    public void search(String query) {
         driver.findElement(By.name("q")).clear();
         driver.findElement(By.name("q")).sendKeys(query + Keys.ENTER);
     }
 
-    private void assertResultsAmount(int resultsAmount) {
-        new WebDriverWait(driver, 6).until(numberOfElementsToBe(By.cssSelector(".srg>.g"), resultsAmount));
+    public static void assertResultsAmount(int resultsAmount) {
+        wait.until(sizeOf(By.cssSelector(".srg>.g"), resultsAmount));
     }
 
-    private void assertFirstResultHasText(String text) {
-        new WebDriverWait(driver, 10).until(textToBePresentInElementLocated(By.cssSelector(".srg>.g:nth-child(1)"), text));
+    public static void assertResult(int index, String text) {
+        wait.until(textToBePresentInElementLocated(By.cssSelector(String.format(".srg>.g:nth-child(%d)", index)), text));
     }
 
-    private void followFirstLink() {
-        driver.findElement(By.cssSelector(".srg>.g:nth-child(1)")).findElement(By.cssSelector(".r>a")).click();
+    public void followLink(int index) {
+        driver.findElements(By.cssSelector(".srg>.g")).get(index).findElement(By.cssSelector(".r>a")).click();
     }
+
+    public static ExpectedCondition<Boolean> sizeOf(final By elementsLocator, final int expectedSize){
+        return new ExpectedCondition<Boolean>() {
+            private int listSize;
+            private List<WebElement> elements;
+
+            public Boolean apply(WebDriver driver) {
+                elements = driver.findElements(elementsLocator);
+                listSize = elements.size();
+                return listSize == expectedSize;
+            }
+
+            public String toString() {
+                return String.format("\nsize of list: %s\n to be: %s\n while actual size is: %s\n", elements, expectedSize, listSize);
+            }
+        };
+    }
+
 
 }
