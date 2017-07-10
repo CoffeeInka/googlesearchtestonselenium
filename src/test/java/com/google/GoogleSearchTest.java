95d6e20ae13c58bced7ccf8639fe75f6b7962c37
CoffeeInka <inna.shevchenko@gmail.com>
2017-07-10 12:38:19 +0200

    CustomCondition is separated into class to src/main/core

--- src/test/java/com/google/GoogleSearchTest.java
+++ src/test/java/com/google/GoogleSearchTest.java
@@ -4,14 +4,11 @@ import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
+import static core.CustomConditions.*;
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 /**
@@ -21,6 +18,7 @@ public class GoogleSearchTest {
 
     public static WebDriver driver;
     public static WebDriverWait wait;
+    public static String results = ".srg>.g";
 
     @BeforeClass
     public static void setUp() {
@@ -41,7 +39,7 @@ public class GoogleSearchTest {
         driver.get("http://google.com");
         search("Selenium automates browsers");
         assertResultsAmount(10);
-        assertResult(1, "for automating web applications for testing purposes");
+        assertResult(0, "for automating web applications for testing purposes");
 
         followLink(0);
         wait.until(urlContains("http://www.seleniumhq.org/"));
@@ -53,33 +51,15 @@ public class GoogleSearchTest {
     }
 
     public void assertResultsAmount(int resultsAmount) {
-        wait.until(sizeOf(By.cssSelector(".srg>.g"), resultsAmount));
+        wait.until(sizeOf(By.cssSelector(results), resultsAmount));
     }
 
     public void assertResult(int index, String text) {
-        wait.until(textToBePresentInElementLocated(By.cssSelector(String.format(".srg>.g:nth-child(%d)", index)), text));
+        wait.until(textToBePresentInElementLocated(By.cssSelector(String.format(".srg>.g:nth-child(%d)", index + 1)), text));
     }
 
     public void followLink(int index) {
-        driver.findElements(By.cssSelector(".srg>.g")).get(index).findElement(By.cssSelector(".r>a")).click();
+        driver.findElements(By.cssSelector(results)).get(index).findElement(By.cssSelector(".r>a")).click();
     }
-
-    public static ExpectedCondition<Boolean> sizeOf(final By elementsLocator, final int expectedSize) {
-        return new ExpectedCondition<Boolean>() {
-            private int listSize;
-            private List<WebElement> elements;
-
-            public Boolean apply(WebDriver driver) {
-                elements = driver.findElements(elementsLocator);
-                listSize = elements.size();
-                return listSize == expectedSize;
-            }
-
-            public String toString() {
-                return String.format("\nsize of list: %s\n to be: %s\n while actual size is: %s\n", elements, expectedSize, listSize);
-            }
-        };
-    }
-
 
 }
